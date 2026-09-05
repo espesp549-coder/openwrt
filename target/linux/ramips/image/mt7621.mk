620973e7723e572b0295e5f79f75b4a6ffdf97a6
espesp549-coder <espesp549@gmail.com>
2026-09-05 17:24:11 +0300

    Update mt7621.mk
    Signed-off-by: espesp549-coder <espesp549@gmail.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -4006,17 +4006,21 @@ define Device/zyxel_wsm20
 endef
 TARGET_DEVICES += zyxel_wsm20
 
-define Device/360t6m
+define Device/360_t6m
   $(Device/nand)
-  $(Device/uimage-lzma-loader)
-  BLOCKSIZE := 128k
-  PAGESIZE := 2048
-  IMAGE_SIZE := 124416k
   DEVICE_VENDOR := 360
   DEVICE_MODEL := T6M
-  DEVICE_PACKAGES := kmod-mt7603 kmod-mt7615-firmware
+  DEVICE_DTS := mt7621_qihoo_360t6m
+  SOC := mt7621
+  KERNEL_SIZE := 4096k
+  IMAGE_SIZE := 124416k
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_LOADADDR := 0x80001000
+  KERNEL := kernel-bin | append-dtb | lzma | uImage lzma
+  DEVICE_PACKAGES := kmod-mt76-connac kmod-mt76-core kmod-mt7915-firmware kmod-mt7915e kmod-tun kmod-nft-tproxy wpad-openssl luci luci-ssl dropbear
   IMAGES += factory.bin
-  IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | \
-	append-ubi | check-size
+  IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | check-size
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += 360t6m
+TARGET_DEVICES += 360_t6m
